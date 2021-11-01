cc6c8c6d7eaaf33de3ca7d0a364dae492a620af8
GPATEL419 <gpatel@timelessveterinary.com>
2021-11-01 15:42:16 -0300

    Type Speed Completed in Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+            <h1>Web development and Website Promotion</h1>
+            <Typed
+                className="typed-text"
+                strings={["Web Design", "Web Development", "Laravel Development", "React JS"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+            />
+            </div>
+        </div>
+    )
+}
+
+export default Header

0ec900877e72b48cc915c8c811fb2774adee0300
GPATEL419 <gpatel@timelessveterinary.com>
2021-11-01 16:12:40 -0300

    particles error

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
                 backSpeed={60}
                 loop
             />
+            <a href="#" className="btn-main-header text-uppercase">Contact me</a>
             </div>
         </div>
     )
